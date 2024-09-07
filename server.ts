7392fd5391fe29da76638b23296cbf6dd0bb288f
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-09-07 14:16:18 -0300

    separate authenticateController from the server.ts file

--- server.ts
+++ server.ts
@@ -4,9 +4,9 @@ import cookieParser from 'cookie-parser'
 import { routes } from './routes/users.routes'
 import { routesCards } from './routes/cards.routes'
 import dotEnv from './verifyDotEnv'
-import { knexConnection } from './connectionDatabase'
 import cors from "cors"
 import { routerTags } from './routes/tags.routes'
+import authentication from "./controllers/authenticationController"
 
 interface User {
   id: number | string;
@@ -84,48 +84,7 @@ export function verifyAuth (req: Request, res: Response, next: NextFunction): vo
 //   next()
 // }
 
-app.post('/auth', async (req, res) => {
-  console.log(req.body)
-  
-  const { email, password } = req.body
-
-  if (!email || !password) {
-    res.status(400).json({
-      message: 'User and password need to be completed'
-    })
-  } else {
-    const user = await knexConnection('Users').where({
-      email,
-      password,
-    }).first()
-
-    if (user) {
-    
-      jwt.sign({ id: user.id, email: user.email, name: user.name, profileImageUrl: user.profileImageUrl }, secretJWT, {
-        expiresIn: '24h'
-      }, (error, token) => {
-      
-        if (error) {
-          return res.status(400).json({
-            message: 'internal error'
-          })
-        }
-        
-        res.cookie('token', token, {
-          //httpOnly: true,
-          maxAge: 1000 * 60 * 60 // 1 hora autenticado
-
-        })
-        
-        res.status(200).redirect('/')
-      })
-    } else {
-      return res.status(400).json({
-        message: 'email or password incorrect'
-      })
-    }
-  }
-})
+app.post('/auth', authentication.authenticationController)
 
 app.use('/api', routes)
 app.use('/api/cards',routesCards)
